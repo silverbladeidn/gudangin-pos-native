d3e70f37c6c1ef011530e5881809f39f5843868f
Muhammad Rifqi <muhammad45rifki@gmail.com>
2026-08-01 07:40:27 +0700

    update

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 include "config.php";
 
 if($_GET['act'] == 'product'){
-    $sql = mysqli_query($koneksi,"select * from product");
+    $sql = mysqli_query($koneksi,"select * from products");
     $rows = array();
     while($data = mysqli_fetch_array($sql)){
         $rows[] = $data;
