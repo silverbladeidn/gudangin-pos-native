54233164f8721e680ebd326a34ed3d6b7eb66124
muhammad-rifqi <muhammad45rifki@gmail.com>
2026-08-06 09:02:14 +0700

    update

--- laporan.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include('config.php');
-    $sql = "SELECT * FROM `products` where id = 4";
-    $result = mysqli_query($config, $sql);
-    $row = mysqli_fetch_assoc($result);
-    echo $row['name'];
-    ?>
--- pemesanan.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include('config.php');
-    $sql = "SELECT * FROM `products` where id = 4";
-    $result = mysqli_query($config, $sql);
-    $row = mysqli_fetch_assoc($result);
-    echo $row['name'];
-    ?>
-    <input type="number" name="jumlah_pesananan">
-    <button onclick="window.location.href = 'laporan.php';">Beli</button>
